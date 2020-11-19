1f70ff1aff3c41bcd00b5a12f3632c655c57843c
Ahmad Chata <ahmadchata@gmail.com>
2020-11-19 12:01:45 +0100

    Add navbar and sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_param)
+    if @user.save
+      flash[:notice] = 'Successful'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Failed'
+      render 'new'
+    end
+  end
+
+  private
+  def user_param
+    params.require(:user).permit(:name)
+  end
 end
