a8c98a473301ce0ec560e9d6bd0336e1f56bcdb4
Ahmad Chata <ahmadchata@gmail.com>
2020-11-19 13:30:30 +0100

    Add user_id to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,27 @@
 class EventsController < ApplicationController
-  # def index
-  #   @event = Event.all
-  # end
+  include EventsHelper
+  def index
+    @event = Event.all
+  end
 
   def new
     @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Event '#{@event.name}' created!"
+      redirect_to @event
+    else
+      flash[:alert] = "Some error!"
+      render 'new'
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_213641) do
+ActiveRecord::Schema.define(version: 2020_11_19_122848) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_213641) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
