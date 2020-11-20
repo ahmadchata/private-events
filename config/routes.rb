c4792863f9ba943a702d1d680f2d59fcb26621f9
Ahmad Chata <ahmadchata@gmail.com>
2020-11-20 16:43:05 +0100

    Modify event controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new', as: 'signup'
   get 'login' => 'sessions#new', as: 'login'
   get 'logout' => 'sessions#destroy', as: 'logout'
-  get 'attend' => 'events#attend', as: 'attend'
-  get 'cancel_attend' => 'events#cancel_attend', as: 'cancel_attend'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :events
   resources :sessions, only: [:new, :create, :destroy]
