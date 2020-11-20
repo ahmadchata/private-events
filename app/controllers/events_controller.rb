6c9b510d44a20b16f26440db86c322510b9bded1
Ahmad Chata <ahmadchata@gmail.com>
2020-11-20 15:14:26 +0100

    Add attendance method to event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,4 +24,20 @@ class EventsController < ApplicationController
       render 'new'
     end
   end
+
+  def attendance
+    @event = Event.find(params[:id])
+    if @event.attendees.include?(current_user)
+      redirect_to @event, notice: "You are already on the list"
+    else
+      @event.attendees << current_user
+      redirect_to @event
+    end
+  end
+
+  def cancel_attendance
+    @event = Event.find(params[:id])
+    @event.attendees.delete(current_user)
+    redirect_to @event, notice: "You are no longer attending this event"
+  end
 end
